680dd99e69a5aeea7be41c8a60af39d79b0dda1b
IT-1-F-30 <135986508+IT-1-F-30@users.noreply.github.com>
2024-11-13 12:58:30 +0900

    aaa

--- test-11.py
+++ test-11.py
@@ -1,6 +1,6 @@
 import cv2
 import mediapipe as mp
-import pyautogui
+# import pyautogui
 import time
 
 kando = 10
@@ -25,7 +25,7 @@ frame_interval = 2
 playing = True
 
 #pygiuの初期設定
-pyautogui.FAILSAFE=False
+# pyautogui.FAILSAFE=False
 
 while True:
     if playing:
@@ -62,24 +62,25 @@ while True:
 
                         dx_ave_left_d = count_left_d[0][0]+count_left_d[1][0]+count_left_d[2][0]+count_left_d[3][0]+count_left_d[4][0]+count_left_d[5][0]+count_left_d[6][0]+count_left_d[7][0]+count_left_d[8][0]+count_left_d[9][0]+count_left_d[10][0]+count_left_d[11][0]+count_left_d[12][0]+count_left_d[13][0]+count_left_d[14][0]+count_left_d[15][0]+count_left_d[16][0]+count_left_d[17][0]+count_left_d[18][0]+count_left_d[19][0]//20
                         dy_ave_left_d = count_left_d[0][1]+count_left_d[1][1]+count_left_d[2][1]+count_left_d[3][1]+count_left_d[4][1]+count_left_d[5][1]+count_left_d[6][1]+count_left_d[7][1]+count_left_d[8][1]+count_left_d[9][1]+count_left_d[10][1]+count_left_d[11][1]+count_left_d[12][1]+count_left_d[13][1]+count_left_d[14][1]+count_left_d[15][1]+count_left_d[16][1]+count_left_d[17][1]+count_left_d[18][1]+count_left_d[19][1]//20
+                        print(count_left_d, count_left_c)
                         #pyautogui.move(-1*count_left_d[7][0]*bai, count_left_d[7][1]*bai)
-                        print(dx_ave_left_d,dy_ave_left_d)
-                        pyautogui.move(-1*dx_ave_left_d*bai_a, dy_ave_left_d*bai_a)
+                        # print(dx_ave_left_d,dy_ave_left_d)
+                        # pyautogui.move(-1*dx_ave_left_d*bai_a, dy_ave_left_d*bai_a)
 
 
                         #print(abs(count_left_c[3][0] - count_left_c[11][0]))
 
-                        if (abs(count_left_c[3][0] - count_left_c[11][0]) <= kando and abs(count_left_c[3][1] - count_left_c[11][1]) <= kando)and(abs(count_left_c[3][0] - count_left_c[15][0]) >= kando and abs(count_left_c[3][1] - count_left_c[15][1]) >= kando):
-                            pyautogui.click()
-                            print("左クリック")
+                        # if (abs(count_left_c[3][0] - count_left_c[11][0]) <= kando and abs(count_left_c[3][1] - count_left_c[11][1]) <= kando)and(abs(count_left_c[3][0] - count_left_c[15][0]) >= kando and abs(count_left_c[3][1] - count_left_c[15][1]) >= kando):
+                        #     pyautogui.click()
+                        #     print("左クリック")
                             
-                        if (abs(count_left_c[3][0] - count_left_c[15][0]) <= kando and abs(count_left_c[3][1] - count_left_c[15][1]) <= kando)and(abs(count_left_c[3][0] - count_left_c[11][0]) >= kando and abs(count_left_c[3][1] - count_left_c[11][1]) >= kando):
-                            pyautogui.rightClick()
-                            print("右クリック")
+                        # if (abs(count_left_c[3][0] - count_left_c[15][0]) <= kando and abs(count_left_c[3][1] - count_left_c[15][1]) <= kando)and(abs(count_left_c[3][0] - count_left_c[11][0]) >= kando and abs(count_left_c[3][1] - count_left_c[11][1]) >= kando):
+                        #     pyautogui.rightClick()
+                        #     print("右クリック")
 
-                        if (abs(count_left_c[3][0] - count_left_c[15][0]) <= kando and abs(count_left_c[3][0] - count_left_c[11][0]) <= kando) and(abs(count_left_c[3][1] - count_left_c[15][1]) <= kando and abs(count_left_c[3][1] - count_left_c[11][1]) <= kando):
-                            pyautogui.doubleClick()
-                            print("ダブルクリック")
+                        # if (abs(count_left_c[3][0] - count_left_c[15][0]) <= kando and abs(count_left_c[3][0] - count_left_c[11][0]) <= kando) and(abs(count_left_c[3][1] - count_left_c[15][1]) <= kando and abs(count_left_c[3][1] - count_left_c[11][1]) <= kando):
+                        #     pyautogui.doubleClick()
+                        #     print("ダブルクリック")
 
                         count_left_d = []
                         count_left_c = []
@@ -97,20 +98,20 @@ while True:
                         dx_ave_right_d = count_right_d[0][0]+count_right_d[1][0]+count_right_d[2][0]+count_right_d[3][0]+count_right_d[4][0]+count_right_d[5][0]+count_right_d[6][0]+count_right_d[7][0]+count_right_d[8][0]+count_right_d[9][0]+count_right_d[10][0]+count_right_d[11][0]+count_right_d[13][0]+count_right_d[14][0]+count_right_d[15][0]+count_right_d[16][0]+count_right_d[17][0]+count_right_d[18][0]+count_right_d[19][0]//20
                         dy_ave_right_d = count_right_d[0][1]+count_right_d[1][1]+count_right_d[2][1]+count_right_d[3][1]+count_right_d[4][1]+count_right_d[5][1]+count_right_d[6][1]+count_right_d[7][1]+count_right_d[8][1]+count_right_d[9][1]+count_right_d[10][1]+count_right_d[11][1]+count_right_d[13][1]+count_right_d[14][1]+count_right_d[15][1]+count_right_d[16][1]+count_right_d[17][1]+count_right_d[18][1]+count_right_d[19][1]//20
                         #pyautogui.move(-1*count_right_d[7][0]*bai, count_right_d[7][1]*bai)
-                        print(dx_ave_right_d,dy_ave_right_d)
-                        pyautogui.move(-1*dx_ave_right_d*bai_a, dy_ave_right_d*bai_a)
+                        # print(dx_ave_right_d,dy_ave_right_d)
+                        # pyautogui.move(-1*dx_ave_right_d*bai_a, dy_ave_right_d*bai_a)
 
-                        if (abs(count_right_c[3][0] - count_right_c[11][0]) <= kando and abs(count_right_c[3][1] - count_right_c[11][1]) <= kando)and(abs(count_right_c[3][0] - count_right_c[15][0]) >= kando and abs(count_right_c[3][1] - count_right_c[15][1]) >= kando):
-                            pyautogui.click()
-                            print("左クリック")
+                        # if (abs(count_right_c[3][0] - count_right_c[11][0]) <= kando and abs(count_right_c[3][1] - count_right_c[11][1]) <= kando)and(abs(count_right_c[3][0] - count_right_c[15][0]) >= kando and abs(count_right_c[3][1] - count_right_c[15][1]) >= kando):
+                        #     pyautogui.click()
+                        #     print("左クリック")
                             
-                        if (abs(count_right_c[3][0] - count_right_c[15][0]) <= kando and abs(count_right_c[3][1] - count_right_c[15][1]) <= kando)and(abs(count_right_c[3][0] - count_right_c[11][0]) >= kando and abs(count_right_c[3][1] - count_right_c[11][1]) >= kando):
-                            pyautogui.rightClick()
-                            print("右クリック")
+                        # if (abs(count_right_c[3][0] - count_right_c[15][0]) <= kando and abs(count_right_c[3][1] - count_right_c[15][1]) <= kando)and(abs(count_right_c[3][0] - count_right_c[11][0]) >= kando and abs(count_right_c[3][1] - count_right_c[11][1]) >= kando):
+                        #     pyautogui.rightClick()
+                        #     print("右クリック")
 
-                        if (abs(count_right_c[3][0] - count_right_c[15][0]) <= kando and abs(count_right_c[3][0] - count_right_c[11][0]) <= kando) and(abs(count_right_c[3][1] - count_right_c[15][1]) <= kando and abs(count_right_c[3][1] - count_right_c[11][1]) <= kando):
-                            pyautogui.doubleClick()
-                            print("ダブルクリック")
+                        # if (abs(count_right_c[3][0] - count_right_c[15][0]) <= kando and abs(count_right_c[3][0] - count_right_c[11][0]) <= kando) and(abs(count_right_c[3][1] - count_right_c[15][1]) <= kando and abs(count_right_c[3][1] - count_right_c[11][1]) <= kando):
+                        #     pyautogui.doubleClick()
+                        #     print("ダブルクリック")
 
                         count_right_d = []
                         count_right_c = []
